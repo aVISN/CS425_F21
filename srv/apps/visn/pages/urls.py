80270c79123d7b456b1342f55c8439b2318239f2
Sarah Skidmore <obeytheviszla@gmail.com>
2021-11-11 13:30:32 -0800

    Added registration page

--- srv/apps/visn/pages/urls.py
+++ srv/apps/visn/pages/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import HomePageView, AboutPageView
+from .views import HomePageView, AboutPageView, RegisterView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('', include('django.contrib.auth.urls')),
     path('about/', AboutPageView.as_view(), name='about'),
+    path('register/', RegisterView.as_view(), name='register'),
 ]
--- srv/apps/visn/pages/views.py
+++ srv/apps/visn/pages/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
 
 class AboutPageView(TemplateView):
     template_name = 'about.html'
+
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/register.html'
