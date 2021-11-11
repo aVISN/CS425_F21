4faa50662083189a6ec5369adf6d7459ed393ad4
Sarah Skidmore <obeytheviszla@gmail.com>
2021-11-11 13:45:46 -0800

    Added django crispy forms bootstrap5 to login and registration forms

--- srv/apps/visn/visn/settings.py
+++ srv/apps/visn/visn/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'pages',
 ]
 
@@ -124,3 +126,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
