0f39d2d01036482fad82e928a6427a31b605be87
Sarah Skidmore <obeytheviszla@gmail.com>
2021-11-11 13:22:55 -0800

    Cleaned up auth app urls

--- srv/apps/visn/pages/urls.py
+++ srv/apps/visn/pages/urls.py
@@ -3,6 +3,6 @@ from .views import HomePageView, AboutPageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('', include('django.contrib.auth.urls')),
     path('about/', AboutPageView.as_view(), name='about'),
-    path('accounts/', include('django.contrib.auth.urls')),
 ]
