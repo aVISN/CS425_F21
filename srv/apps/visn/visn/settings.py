88e5a8c7073cefbdebc448aa286badf6e320041d
Sarah Skidmore <obeytheviszla@gmail.com>
2021-11-11 23:49:59 -0800

    Revoked project secret key and rotated it

--- srv/apps/visn/visn/settings.py
+++ srv/apps/visn/visn/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config # for secret key storage
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hqd!_vdkkwxm@4um4h4gj+(@1ac#)niv9=j@etqt*05@-$&uif'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
