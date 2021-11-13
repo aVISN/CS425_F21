8aa91a35f9b095c69822efea597ae44a8d5b9ce5
Sarah Skidmore <obeytheviszla@gmail.com>
2021-11-13 14:28:12 -0800

    Updated login and logout to redirect to homepage

--- srv/apps/visn/visn/settings.py
+++ srv/apps/visn/visn/settings.py
@@ -130,3 +130,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
