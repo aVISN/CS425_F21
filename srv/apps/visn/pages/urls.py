82a034a2d14d5f3e6de29216436a59737163cd6e
Logicupgrade <haydend4@gmail.com>
2021-11-27 01:19:15 -0800

    added projects, files, and chat

--- srv/apps/visn/pages/urls.py
+++ srv/apps/visn/pages/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path, include
-from .views import LoginPageView, HomePageView, AboutPageView, RegisterView, DashboardView
+from .views import LoginPageView, HomePageView, AboutPageView, ProjectsPageView, FilesPageView, ChatPageView, RegisterView, DashboardView
 
 urlpatterns = [
     path('', LoginPageView.as_view(), name='home'),
 #   path('', HomePageView.as_view(), name='home'),
     path('', include('django.contrib.auth.urls')),
     path('about/', AboutPageView.as_view(), name='about'),
+    path('projects/', ProjectsPageView.as_view(), name='about'),
+    path('files/', FilesPageView.as_view(), name='about'),
+    path('chat/', ChatPageView.as_view(), name='about'),
     path('register/', RegisterView.as_view(), name='register'),
     path('dashboard/', DashboardView.as_view(), name='dashboard'),
 ]
