5e79da392d53903bbf1844e63e2711559d541b1b
logicupgrade <haydend4@gmail.com>
2021-12-07 15:40:04 -0800

    added network html css url view

--- srv/apps/visn/pages/urls.py
+++ srv/apps/visn/pages/urls.py
@@ -1,7 +1,7 @@
 # docs: https://docs.djangoproject.com/en/3.2/topics/http/urls/
 
 from django.urls import path, include
-from .views import ProjectsPageView, ChatPageView, RegisterView, DashboardView, FilesPageView, ToolsPageView, UploadFilesView #HomePageView, AboutPageView,  
+from .views import ProjectsPageView, ChatPageView, RegisterView, DashboardView, NetworkPageView, FilesPageView, ToolsPageView, UploadFilesView #HomePageView, AboutPageView,  
 from . import views
 from django.contrib.auth import views as auth_views
 from django.conf import settings
@@ -16,6 +16,7 @@ urlpatterns = [
     path('chat/', ChatPageView.as_view(), name='chat'),
     path('register/', RegisterView.as_view(), name='register'),
     path('dashboard/', DashboardView.as_view(), name='dashboard'),
+    path('network/', NetworkPageView.as_view(), name='network'),
     path('files/', FilesPageView.as_view(), name='files'),
     path('tools/', ToolsPageView.as_view(), name='tools'),
     path('files/upload/', UploadFilesView.as_view(), name='upload'),
